0d0f16f67a7f605b97b2a16e910998026c514088
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-05-28 14:31:40 -0300

    Persistindo banco de dados

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,22 @@
-export type User = {
-  name: string;
-  age: string;
-};
+import fs from "node:fs/promises";
+
+const databasePath = new URL("db.json", import.meta.url);
 
 export class Database {
   private database: Record<string, unknown[]> = {};
+  private constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.persist();
+      });
+  }
+
+  private persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.database));
+  }
 
   public select<T>(table: string): T[] {
     const data = this.database[table] ?? [];
@@ -17,6 +29,7 @@ export class Database {
     } else {
       this.database[table] = [data];
     }
+    this.persist();
 
     return data;
   }
